bdf7778fd982f34e22ada2aae3fe44a6e82ed344
Jon-AL <yalexand@uwo.ca>
2025-02-24 05:19:00 -0500

    Added more information and links to README.md. A few basic improvements. Added a warning addressing web deployment.

--- front/home_window.py
+++ front/home_window.py
@@ -2,16 +2,26 @@ import flet as ft
 
 from utils.path_utils import PathManager, PathFlag
 
-text = open(
-    PathManager.resolve_path('README.md', PathFlag.R)
-    , 'r').read()
-
 
 def home_window():
+    text = open(
+        PathManager.resolve_path('README.md', PathFlag.R)
+        , 'r').read()
+    r = ft.Row([ft.Icon(ft.Icons.WARNING_AMBER, color=ft.Colors.AMBER),
+                ft.Text("Warning\nSome features are not available for web deployment. Yet.",
+                        theme_style=ft.TextThemeStyle.BODY_LARGE, text_align=ft.TextAlign.CENTER),
+                ft.Icon(ft.Icons.WARNING_AMBER, color=ft.Colors.AMBER),
+                ], alignment=ft.MainAxisAlignment.CENTER)
+
+    c = ft.Container(r, bgcolor=ft.Colors.with_opacity(0.3, ft.Colors.AMBER), alignment=ft.Alignment(0, 0))
+
     return ft.Container(
         content=ft.Column([
+            c,
+            ft.Text("WELCOME!", theme_style=ft.TextThemeStyle.HEADLINE_LARGE),
             ft.Markdown(text)
-        ], spacing=10),
+        ], spacing=10,
+            scroll=ft.ScrollMode.AUTO),
         expand=True,
         padding=20,
         border=ft.border.all(1, ft.Colors.BLACK12),
